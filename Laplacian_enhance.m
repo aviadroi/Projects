1b9e58f0d8bac444caa88f4278940fca5358a472
aviadroi <80147903+aviadroi@users.noreply.github.com>
2021-06-23 11:43:17 +0300

    Update and rename ex_3d_1.m to Laplacian_enhance.m

--- Laplacian_enhance.m
+++ Laplacian_enhance.m
@@ -1,10 +1,14 @@
+% Implementing the Laplacian enhancement technique.
+
 clear all; close all; clc;
 
 % Read a RGB image
 A = imread('fig3.38a.jpg');
 A_gray = rgb2gray(A);
 
+% Laplacian without diagonal elements:
 w=[0 1 0; 1 -4 1; 0 1 0];
+% Laplacian with diagonal elements:
 w2=[1 1 1; 1 -8 1; 1 1 1];
 
 Am=zeros(size(A_gray,1)+2,size(A_gray,2)+2);
@@ -24,7 +28,7 @@ Ag(2:end-1,2:end-1)=w2(1,1)*Am(1:end-2,1:end-2)+w2(1,2)*Am(1:end-2,2:end-1)+w2(1
 +w2(3,1)*Am(3:end,1:end-2)+w2(3,2)*Am(3:end,2:end-1)+w2(3,3)*Am(3:end,3:end);
 
 Af1=((Af-min(min(Af)))*255/(max(max(Af))-min(min(Af))));
-% possible also:
+% also possible:
 % Af1=(Af-min(min(Af)));
 % Af1=Af1/max(max(Af1))*255;
 Ag1=((Ag-min(min(Ag)))*255/(max(max(Ag))-min(min(Ag))));
@@ -33,7 +37,7 @@ Af2=Am-Af;
 Ag2=Am-Ag;
 
 Am=uint8(Am);
-Afu=uint8(Af);
+% Afu=uint8(Af);
 Afs=uint8(Af1);
 Ags=uint8(Ag1);
 Af2=uint8(Af2);
